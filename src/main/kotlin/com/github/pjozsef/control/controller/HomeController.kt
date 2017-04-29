0cacfa8e6cde1d5b43c60694e5c2a2e3283f29e7
jpollak <jozsef.pollak@emarsys.com>
2017-04-29 21:04:25 +0200

    feat(control): add secret token as default header

--- src/main/kotlin/com/github/pjozsef/control/controller/HomeController.kt
+++ src/main/kotlin/com/github/pjozsef/control/controller/HomeController.kt
@@ -3,4 +3,5 @@ package com.github.pjozsef.control.controller
 class HomeController(
         host: String,
         port: Int,
-        client: ControllerService = ServiceFactory.create(host, port)): ControllerService by client
+        secret: String,
+        client: ControllerService = ServiceFactory.create(host, port, secret)): ControllerService by client
--- src/main/kotlin/com/github/pjozsef/control/controller/ServiceFactory.kt
+++ src/main/kotlin/com/github/pjozsef/control/controller/ServiceFactory.kt
@@ -1,18 +1,34 @@
 package com.github.pjozsef.control.controller
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ServiceFactory {
-    companion object{
-        fun create(host: String, port: Int): ControllerService {
-            val retrofit = Retrofit.Builder()
-                    .baseUrl("http://$host:$port/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+    companion object {
+        fun create(host: String, port: Int, secret: String): ControllerService =
+                Retrofit.Builder()
+                        .baseUrl("http://$host:$port/")
+                        .client(okhttp(secret))
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                        .build()
+                        .create(ControllerService::class.java)
+
+        private fun okhttp(secret: String): OkHttpClient =
+                OkHttpClient.Builder()
+                        .addInterceptor(authInterceptor(secret))
+                        .build()
+
+        private fun authInterceptor(secret: String) = Interceptor { chain ->
+            val newRequest = chain
+                    .request()
+                    .newBuilder()
+                    .addHeader("token", secret)
                     .build()
-            return retrofit.create(ControllerService::class.java)
+            chain.proceed(newRequest)
         }
     }
 }
